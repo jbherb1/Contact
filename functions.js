fd7fd611f069dc5ba447637204bf06c542dbf63f
Jordan Herbert <jbherbwebdesign@gmail.com>
2019-05-17 14:57:55 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,18 @@
+function picture(){
+//picture of me
+
+}
+
+function instagram(){
+//href link to my instagram page
+
+}
+
+function email(){
+//open link adding email
+
+}
+
+function info(){
+//paragraph about me.
+}
---
+++ sketch.js
@@ -0,0 +1,10 @@
+let img,paper;
+function setup() {
+let img=createImg('jordan.jpeg');
+img.size(273,410);
+img.position(windowWidth/7,windowHeight/30);
+}
+
+function draw() {
+
+}
